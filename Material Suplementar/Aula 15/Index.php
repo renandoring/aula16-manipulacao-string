e35d4da392212d7256f8b0cb339e4c281fbab92b
rdoring <rdoring@rdoring.(none)>
2014-03-01 19:50:01 -0300

    aula16

---
+++ Material Suplementar/Aula 15/Index.php
@@ -0,0 +1,20 @@
+﻿<?php
+	// Cria uma variável para armazenar o caminho do arquivo a ser copiado
+	$arquivo = 'D:/copiarEmJava.txt';
+	// Cria uma variável para armazenar o caminho no qual o arquivo deve ser copiado
+	$destino = 'C:/Users/Anderson/Desktop/copiarEmJavaCopy.txt';
+	// Verifica se o arquivo informado existe
+	if(file_exists($arquivo)){
+		// Se o arquivo existir copia o arquivo
+		if(copy($arquivo,$destino)){
+			// Exibe uma mensagem de sucesso
+			echo 'Arquivo copiado com sucesso';
+		}else{
+			// Caso ocorra um erro, exibe uma mensagem de erro
+			echo 'Erro ao copiar';
+		}
+	}else{
+		// Caso o arquivo não exista, exibe uma mensagem de erro
+		echo "O arquivo não existe";
+	}
+?>
---
+++ Material Suplementar/Aula 16/Index.php
@@ -0,0 +1,12 @@
+﻿<?php
+	// Cria uma variável e armazena uma conteúdo para ser preenchido
+	$minhaString = 'Conteudo';
+	// Cria uma variável para armazenar a string que irá preencher a string criada
+	$preencher = '%';
+	// Preenche a string com 20 caracteres, ou seja, 12 vezes o %, começando pelo lado direito
+	echo str_pad($minhaString,20,$preencher).'<br>';
+	// Preenche a string com 20 caracteres, ou seja, 12 vezes o %, começando pelo lado esquerdo
+	echo str_pad($minhaString,20,$preencher,STR_PAD_LEFT).'<br>';
+	// Preenche a string com 20 caracteres, ou seja, 12 vezes o %, de ambos os lados
+	echo str_pad($minhaString,20,$preencher,STR_PAD_BOTH).'<br>';
+?>
